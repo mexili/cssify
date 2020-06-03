3ed2d6b2f125c3c8e1a122b1db840cd0d295c214
stealthanthrax <sansyrox@gmail.com>
2020-06-04 02:10:20 +0530

    Add store and live preview demo

--- client/src/containers/styled.jsx
+++ client/src/containers/styled.jsx
@@ -1,4 +1,7 @@
 import styled, { css } from "styled-components";
+import {blue} from '../store.js';
+import {useRecoilState} from 'recoil';
+import React from 'react';
 
 const myCSS = css`
   background: ${({ myColor }) => myColor || `black`};
@@ -7,15 +10,26 @@ const myCSS = css`
 const MyComponent = styled("div")`
   ${myCSS};
 `;
-const Button = styled.button`
-  background: ${({ bgColor }) => bgColor || `white`};
-  color: ${({ color }) => color || `black`};
 
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-`;
+
+const Button = ()=>{
+  const [bgcolor, setBgColor] = useRecoilState(blue);
+
+  let styles = {
+    'background': bgcolor,
+    'color': ({ color }) => color || `black`,
+    'fontSize': '1em',
+    'margin': '1em',
+    'padding': '0.25em 1em',
+    'border': '2px solid palevioletred',
+    'borderRadius': '3px'
+  }
+
+  return (
+    <button style={styles} onClick={()=>setBgColor('blue')} />
+  )
+
+
+}
 
 export { MyComponent, Button };
---
+++ client/src/store.js
@@ -0,0 +1,218 @@
+import {atom} from "recoil";
+
+
+export const mobile = atom({
+    key: "mobile",
+    default: "1"
+});
+
+export const tablet = atom({
+    key: "tablet",
+    default: "1"
+});
+
+export const desktop = atom({
+    key: "desktop",
+    default: "1"
+});
+
+export const widescreen = atom({
+    key: "widescreen",
+    default: "1"
+});
+
+export const fullhd = atom({
+    key: "fullhd",
+    default: "1"
+});
+
+export const grid_tc = atom({
+    key: "grid-tc",
+    default: "1"
+});
+
+export const grid_tr = atom({
+    key: "grid-tr",
+    default: "1"
+});
+
+export const col_gap = atom({
+    key: "col-gap",
+    default: "1"
+});
+
+export const row_gap = atom({
+    key: "row-gap",
+    default: "1"
+});
+
+export const grid_cs = atom({
+    key: "grid-cs",
+    default: "1"
+});
+
+export const grid_ce = atom({
+    key: "grid-ce",
+    default: "1"
+});
+
+export const grid_rs = atom({
+    key: "grid-rs",
+    default: "1"
+});
+
+export const grid_re = atom({
+    key: "grid-re",
+    default: "1"
+});
+
+export const navy = atom({
+    key: "navy",
+    default: "1"
+});
+
+export const blue = atom({
+    key: "blue",
+    default: ""
+});
+
+export const aqua = atom({
+    key: "aqua",
+    default: "1"
+});
+
+export const teal = atom({
+    key: "teal",
+    default: "1"
+});
+
+export const olive = atom({
+    key: "olive",
+    default: "1"
+});
+
+export const green = atom({
+    key: "green",
+    default: "1"
+});
+
+export const lime = atom({
+    key: "lime",
+    default: "1"
+});
+
+export const yellow = atom({
+    key: "yellow",
+    default: "1"
+});
+
+export const orange = atom({
+    key: "orange",
+    default: "1"
+});
+
+export const red = atom({
+    key: "red",
+    default: "1"
+});
+
+export const fuchsia = atom({
+    key: "fuchsia",
+    default: "1"
+});
+
+export const purple = atom({
+    key: "purple",
+    default: "1"
+});
+
+export const maroon = atom({
+    key: "maroon",
+    default: "1"
+});
+
+export const white = atom({
+    key: "white",
+    default: "1"
+});
+
+export const gray = atom({
+    key: "gray",
+    default: "1"
+});
+
+export const silver = atom({
+    key: "silver",
+    default: "1"
+});
+
+export const black = atom({
+    key: "black",
+    default: "1"
+});
+
+export const link_dark = atom({
+    key: "link-dark",
+    default: "1"
+});
+
+export const link_light = atom({
+    key: "link-light",
+    default: "1"
+});
+
+export const link_mid = atom({
+    key: "link-mid",
+    default: "1"
+});
+
+export const sans_font = atom({
+    key: "sans-font",
+    default: "1"
+});
+
+export const serif_font = atom({
+    key: "serif-font",
+    default: "1"
+});
+
+export const utils = atom({
+    key: "utils",
+    default: false
+});
+
+export const res_helpers = atom({
+    key: "res_helpers",
+    default: false
+});
+
+export const grid = atom({
+    key: "grid",
+    default: false
+});
+
+export const forms = atom({
+    key: "forms",
+    default: false
+});
+
+export const type = atom({
+    key: "type",
+    default: false
+});
+
+export const layout = atom({
+    key: "layout",
+    default: false
+});
+
+export const elements = atom({
+    key: "elements",
+    default: false
+});
+
+export const colors = atom({
+    key: "colors",
+    default: false
+});
+
