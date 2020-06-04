0bcc17ae893d43c500e3ee441b34d419f0254b45
Aniket Kumar <aniketkumar049@gmail.com>
2020-06-05 01:21:26 +0530

    deployment script added

---
+++ backend/deploy/setup.sh
@@ -0,0 +1,8 @@
+PROJECT_BASE_PATH='/root/0.4.2-cssify'
+# Configure nginx
+cp $PROJECT_BASE_PATH/deploy/nginx_cssify_api.conf /etc/nginx/sites-available/cssify_api.conf
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/cssify_api.conf /etc/nginx/sites-enabled/cssify_api.conf
+systemctl restart nginx.service
+
+echo "DONE! :)"
