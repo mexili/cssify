ebc6772c0cee524c898917d4b54162933117ecac
Aniket Kumar <aniketkumar049@gmail.com>
2020-06-05 12:09:44 +0530

    color picker added

--- client/src/containers/ReactForm.jsx
+++ client/src/containers/ReactForm.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
+import React from "react";
 import Form from "@rjsf/core";
-import formSchema from '../models/FormSchema'
-import {blue, formSchemaObject} from '../store.js';
-import {useSetRecoilState} from 'recoil';
+import { formSchema, uiSchema } from "../models/FormSchema";
+import { blue, formSchemaObject } from "../store.js";
+import { useSetRecoilState } from "recoil";
 
-import * as _ from 'lodash';
-
+import * as _ from "lodash";
 
 const log = (type) => console.log.bind(console, type);
 
-const ReactForm = ()=> {
-    const  setFormSchemaObject = useSetRecoilState(formSchemaObject);
-    const  setBgColor = useSetRecoilState(blue);
-    const updateForm = _.debounce(async ({formData}, e) => {
-        // if(JSON.stringify(formData)!==JSON.stringify(getFormSchemaObject)) {
-            setFormSchemaObject(formData)
-            console.log(formData)
-        // }
-    })
+const ReactForm = () => {
+  const setFormSchemaObject = useSetRecoilState(formSchemaObject);
+  const setBgColor = useSetRecoilState(blue);
+  const updateForm = _.debounce(async ({ formData }, e) => {
+    // if(JSON.stringify(formData)!==JSON.stringify(getFormSchemaObject)) {
+    setFormSchemaObject(formData);
+    console.log(formData);
+    // }
+  });
 
-
-    let schema = {...formSchema}
-    return (
-        <div>
-            <Form schema={schema}
-                onChange={updateForm}
-                onSubmit={({formData}, e)=>{
-                    e.preventDefault();
-                    setBgColor(formData.blue)
-                    console.log(formData)
-
-                }}
-                onError={log("errors")} />
-        </div>
-    )
-}
+  let schema = { ...formSchema };
+  return (
+    <div>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onChange={updateForm}
+        onSubmit={({ formData }, e) => {
+          e.preventDefault();
+          setBgColor(formData.blue);
+          console.log(formData);
+        }}
+        onError={log("errors")}
+      />
+    </div>
+  );
+};
 
 export default ReactForm;
