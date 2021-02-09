c5d75bf03b28eef9180c6864863e438ee0519d7a
nimit2801 <mnimitsavant@outlook.com>
2021-02-09 12:17:48 +0530

    downloading css files fixed

--- client/src/components/Playground/Playground.jsx
+++ client/src/components/Playground/Playground.jsx
@@ -21,10 +21,11 @@ import Title from "../Minimals/Title";
 import Container from "../Skeleton/Container";
 import Flexbox from "../Skeleton/Flexbox";
 import Table from "../Table/Table";
-import fileDownload from "js-file-download";
+//import fileDownload from "js-file-download";
 import loadingGif from "../../assets/images/loading.gif";
 
 const URL = "http://localhost:8080/";
+const URL2 = "http://localhost:8080/test/";
 
 const Button = () => {
   const formData = useRecoilValue(formSchemaObject);
@@ -41,23 +42,35 @@ const Button = () => {
       onClick={(e) => {
         e.preventDefault();
         setLoading(true);
+
+        console.log(formData.type, "1");
         fetch(URL, {
           method: "POST",
-          body: formData,
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(formData)
         })
           .then((response) => {
-            return response.text();
-          })
-          .then((res) => {
-            fetch(`${URL}${res}`)
-              .then((res) => {
-                return res.text();
+            response.text().then(ress => {
+              console.log(ress);
+              fetch(`${URL}${ress}`)
+                .then(css =>{
+                  css.text().then(style => {
+                    FileDownload(style, "stylesheet.css");
+                    setLoading(false);
+                  });
               })
-              .then((content) => {
-                FileDownload(content, "stylesheet.css");
-              });
-            setLoading(false);
+            });
           })
+          // .then((res) => {
+          //   fetch(`${URL}${res}`)
+          //     .then((res) => {
+          //       return res.text();
+          //     })
+          //     .then((content) => {
+          //       FileDownload(content, "stylesheet.css");
+          //     });
+          //   setLoading(false);
+          // })
           .catch((e) => {
             console.log(e);
             setLoading(false);
